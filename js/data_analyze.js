a0ded675a684dcfe398b9a205957170a472d8aef
lofoz <lofoz.tw@gmail.com>
2020-04-13 23:43:29 +0800

    add new graph and fix display graph

--- js/data_analyze.js
+++ js/data_analyze.js
@@ -2,42 +2,52 @@ $(document).ready(function() {
 	analyze();
 })
 $(".das0").click(function() {
-	data_process(0);
+	analyze(0);
 })
 $(".das1").click(function() {
-	data_process(1);
+	analyze(1);
 })
 $(".das2").click(function() {
-	data_process(2);
+	analyze(2);
 })
 $(".das3").click(function() {
-	data_process(3);
+	analyze(3);
 })
 $(".das4").click(function() {
-	data_process(4);
+	analyze(4);
 })
 $(".das5").click(function() {
-	data_process(5);
+	analyze(5);
 })
 $(".das6").click(function() {
-	data_process(6);
+	analyze(6);
 })
 $(".das7").click(function() {
-	data_process(7);
+	analyze(7);
 })
-var analyze_data;
 
-function analyze() {
+function analyze(sensor = 0) {
 	// 處理 data
-	var xhr = new XMLHttpRequest;
-	var url = "https://nckuairpollution.csie.ncku.edu.tw:6800/campus/get_file";
-	xhr.open('GET', url);
-	xhr.responseType = 'json';
-	xhr.send();
-	xhr.onload = function() {
-		analyze_data = xhr.response;
-		data_process(0);
+	var xhr1 = new XMLHttpRequest;
+	var url1 = "https://nckuairpollution.csie.ncku.edu.tw:6800/requesting/specific/" + sensor;
+	xhr1.open('GET', url1);
+	xhr1.responseType = 'json';
+	xhr1.send();
+	xhr1.onload = function() {
+		let analyze_data = xhr1.response;
+		specific(sensor, analyze_data);
 	}
+
+	var xhr2 = new XMLHttpRequest;
+	var url2 = "https://nckuairpollution.csie.ncku.edu.tw:6800/requesting/prob/" + sensor;
+	xhr2.open('GET', url2);
+	xhr2.responseType = 'json';
+	xhr2.send();
+	xhr2.onload = function() {
+		let analyze_data = xhr2.response;
+		prob(sensor, analyze_data);
+	}
+
 	// xhr.onreadystatechange = ()=>{
 	// 	if(xhr.readyState === XMLHttpRequest.DONE)
 	// 	{
@@ -50,165 +60,133 @@ function analyze() {
 	// };
 }
 
-function data_process(sensor) {
-	var s1 = analyze_data[sensor];
-	// console.log(analyze_data[0])
-	// console.log(s1["pm25hr"])
+function specific(sensor, analyze_data) {
 
-	// 24 小時分析圖
-	var hrtrace1 = {
-		x: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23],
-		y: s1["humhr"],
-		type: 'scatter',
-		mode: 'lines+markers',
-		name: 'humidity'
-	};
-	var hrtrace2 = {
-		x: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23],
-		y: s1["pm10hr"],
-		type: 'scatter',
-		mode: 'lines+markers',
-		name: 'PM10'
-	};
-	var hrtrace3 = {
-		x: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23],
-		y: s1["pm100hr"],
-		type: 'scatter',
-		mode: 'lines+markers',
-		name: 'PM100'
-	};
-	var hrtrace4 = {
-		x: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23],
-		y: s1["pm25hr"],
-		type: 'scatter',
-		mode: 'lines+markers',
-		name: 'PM25'
-	};
-	var hrtrace5 = {
-		x: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23],
-		y: s1["temphr"],
-		type: 'scatter',
-		mode: 'lines+markers',
-		name: 'Temperature'
-	};
+	var data = [],
+		day;
+	for (var i = 0; i < 7; i += 3) {
+		day = {
+			x: ['08:00:00', '12:00:00', '18:00:00'],
+			y: [analyze_data[i]['pm25'], analyze_data[i + 1]['pm25'], analyze_data[i + 2]['pm25']],
+			mode: 'lines+markers',
+			name: analyze_data[i]['date'].substr(0, 10),
+			line: {
+				shape: 'spline',
+			},
+			type: 'scatter'
+		}
+		data.push(day)
+	}
 
-
-	// 一週 分析圖
-	var wktrace1 = {
-		x: ["一", "二", "三", "四", "五", "六", "日"],
-		y: s1["humweek"],
-		type: 'scatter',
-		mode: 'lines+markers',
-		name: 'humidity'
-	};
-	var wktrace2 = {
-		x: ["一", "二", "三", "四", "五", "六", "日"],
-		y: s1["pm10week"],
-		type: 'scatter',
-		mode: 'lines+markers',
-		name: 'PM10'
-	};
-	var wktrace3 = {
-		x: ["一", "二", "三", "四", "五", "六", "日"],
-		y: s1["pm100week"],
-		type: 'scatter',
-		mode: 'lines+markers',
-		name: 'PM100'
-	};
-	var wktrace4 = {
-		x: ["一", "二", "三", "四", "五", "六", "日"],
-		y: s1["pm25week"],
-		type: 'scatter',
-		mode: 'lines+markers',
-		name: 'PM25'
-	};
-	var wktrace5 = {
-		x: ["一", "二", "三", "四", "五", "六", "日"],
-		y: s1["tempweek"],
-		type: 'scatter',
-		mode: 'lines+markers',
-		name: 'Temperature'
-	};
-
-	var hrlayout = {
-		title: '24 hour average sensor 分析圖',
+	var layout = {
+		title: '前三天 特定時間 資料圖',
+		legend: {
+			y: 0.5,
+			traceorder: 'reversed',
+			font: {
+				size: 16
+			},
+			yref: 'paper'
+		},
+		font: {
+			family: 'Microsoft JhengHei, monospace',
+		},
 		xaxis: {
-			title: '小時 hr',
+			title: '時間 Time',
 		},
 		yaxis: {
-			title: '值 value',
+			title: 'μg/m<sup>3</sup>',
 		},
 		width: 520
 	};
-	var wklayout = {
-		title: '一週 sensor 分析圖',
-		xaxis: {
-			title: '一週 one week',
-		},
-		yaxis: {
-			title: '值 value',
-		},
-		width: 520
-	};
-	// 1 hum   2 pm10   3 pm100   4 pm25   5 temp
-	var hrcom = [hrtrace4, hrtrace5];
 	var config = {
 		responsive: true
 	}
-	Plotly.newPlot('graph1' + sensor, hrcom, hrlayout, config);
 
-	var wkcom = [wktrace4, wktrace5]
-	Plotly.newPlot('graph2' + sensor, wkcom, wklayout, config);
+	Plotly.newPlot('graph1' + sensor, data, layout, config);
+}
 
-	var hrcom = [hrtrace4, hrtrace1]
-	Plotly.newPlot('graph3' + sensor, hrcom, hrlayout, config);
+function prob(sensor, analyze_data) {
 
-	var wkcom = [wktrace4, wktrace1]
-	Plotly.newPlot('graph4' + sensor, wkcom, wklayout, config);
+	var y1 = [];
+	var y2 = [];
+	var y3 = [];
+	var y4 = [];
+	var x1 = [];
 
-	var hrcom = [hrtrace2, hrtrace3, hrtrace4]
-	Plotly.newPlot('graph5' + sensor, hrcom, hrlayout, config);
+	for (var i = 0; i < 24; i = i + 1) {
+		x1.push(i);
+		y1.push(analyze_data[i]["green"] * 100);
+		y2.push(analyze_data[i]["yellow"] * 100);
+		y3.push(analyze_data[i]["orange"] * 100);
+		y4.push(analyze_data[i]["red"] * 100);
+	}
 
-	var wkcom = [wktrace2, wktrace3, wktrace4]
-	Plotly.newPlot('graph6' + sensor, wkcom, wklayout, config);
+	var traces = [{
+			x: x1,
+			y: y1,
+			stackgroup: 'one',
+			line: {
+				color: 'green'
+			},
+			name: '0~20'
+		},
+		{
+			x: x1,
+			y: y2,
+			stackgroup: 'one',
+			line: {
+				color: 'yellow'
+			},
+			name: '20~40'
+		},
+		{
+			x: x1,
+			y: y3,
+			stackgroup: 'one',
+			line: {
+				color: 'orange'
+			},
+			name: '40~60'
+		},
+		{
+			x: x1,
+			y: y4,
+			stackgroup: 'one',
+			line: {
+				color: 'red'
+			},
+			name: '60up'
+		}
 
-	// for(var i = 0; i < data.length; i++)
-	// {
-	// 	switch(data[i]["position"])
-	// 	{
-	// 		case 0:
-	// 			s0.push(data[i]);
-	// 		break;
-	// 		case 1:
-	// 			s1.push(data[i]);
-	// 		break;
-	// 		case 2:
-	// 			s2.push(data[i]);
-	// 		break;
-	// 		case 3:
-	// 			s3.push(data[i]);
-	// 		break;
-	// 		case 4:
-	// 			s4.push(data[i]);
-	// 		break;
-	// 		case 5:
-	// 			s5.push(data[i]);
-	// 		break;
-	// 		case 6:
-	// 			s6.push(data[i]);
-	// 		break;
-	// 		case 7:
-	// 			s7.push(data[i]);
-	// 		break;
-	// 		default:
-	// 			console.log(data[i]);
-	// 		break;
-	// 	}
-	// }
-	// console.log("oK");
+	];
 
-	// for(var i = 0; i < s0.length; i++)
-	// {
-	// }
-	// console.log(s0[0]);
+	var layout = {
+		title: 'pm2.5機率圖',
+		legend: {
+			y: 0.5,
+			traceorder: 'reversed',
+			font: {
+				family: 'Microsoft JhengHei, monospace',
+				size: 16
+			},
+			yref: 'paper'
+		},
+		font: {
+			family: 'Microsoft JhengHei, monospace',
+		},
+		xaxis: {
+			title: '時間'
+		},
+		yaxis: {
+			title: 'pm2.5機率值'
+		},
+		width: 520
+	};
+
+	var config = {
+		responsive: true
+	}
+
+	Plotly.newPlot('graph2' + sensor, traces, layout, config);
 }
