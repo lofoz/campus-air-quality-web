27f33b2d233e9543c257a38a34b5109b55b84ac4
lofoz <lofoz.tw@gmail.com>
2020-05-11 08:09:42 +0800

    fix sw.js for cache

--- sw.js
+++ sw.js
@@ -4,7 +4,15 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox
 // 使用precache功能，在offline下也可以執行
 // 要存進cache storage裡的檔案清單
 var cacheFiles = [
-  "./",
+  "./index.html",
+  "./data_analyze.html",
+  "./direction.html",
+  "./css/styles.css",
+  "./js/googlemap.js",
+  "./js/loaddata.js",
+  "./js/data_analyze.js",
+  "./js/direction.js",
+  "./img/ncku-logo-b.png",
   {
     url: './index.html',
     revision: '00000001' // 加revision，版本改了以後，sw.js 在 application 上會更新
