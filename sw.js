3fab7c9b0d5a2cc975e4e64086c346acfa28d8e8
lofoz <lofoz.tw@gmail.com>
2020-05-21 06:35:46 +0800

    add push notification

--- sw.js
+++ sw.js
@@ -1,5 +1,10 @@
 // 引用workbox build
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts('https://www.gstatic.com/firebasejs/7.14.4/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.14.4/firebase-messaging.js');
+
+workbox.clientsClaim();
+workbox.skipWaiting();
 
 // 使用precache功能，在offline下也可以執行
 // 要存進cache storage裡的檔案清單
@@ -20,3 +25,64 @@ var cacheFiles = [
 	}
 ];
 workbox.precaching.precacheAndRoute(cacheFiles);
+
+var click_action; // 點擊notifiction要去的網址
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+	apiKey: "AIzaSyBBnECtCvqhG3wmZpORfS3VJZlgjK1KLaU",
+	authDomain: "test-6d390.firebaseapp.com",
+	databaseURL: "https://test-6d390.firebaseio.com",
+	projectId: "test-6d390",
+	storageBucket: "test-6d390.appspot.com",
+	messagingSenderId: "1072871991195",
+	appId: "1:1072871991195:web:0c4beef2a181536b15633e",
+	measurementId: "G-QSTSCG37TE"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var messaging = firebase.messaging();
+
+
+// firefox bug：在 firebase 掌控所有 notifiction 前，先註冊一個 notificationclick 事件
+// 監聽notifiction點擊事件
+self.addEventListener('notificationclick', function(event) {
+	var url = click_action;
+	console.log(url);
+	event.notification.close();
+	event.waitUntil(
+		clients.matchAll({
+			type: 'window'
+		}).then(windowClients => {
+			// 如果tab是開著的，就 focus 這個tab
+			for (var i = 0; i < windowClients.length; i++) {
+				var client = windowClients[i];
+				if (client.url === url && 'focus' in client) {
+					return client.focus();
+				}
+			}
+			// 如果沒有，就新增tab
+			if (clients.openWindow) {
+				return clients.openWindow(click_action);
+			}
+		})
+	);
+});
+
+
+messaging.setBackgroundMessageHandler(function(payload) {
+	var data = payload.notification;
+	var title = data.title;
+	var options = {
+		body: data.body
+	};
+	click_action = data.click_action;
+
+	return self.registration.showNotification(title, options);
+});
+
+self.addEventListener('notificationclose', function(event) {
+	console.log('使用者關閉')
+});
+
